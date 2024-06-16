238af7c9d78d3efc8a5beee1e2727370fcd0497c
divyaa6 <165681084+divyaa6@users.noreply.github.com>
2024-06-16 16:36:00 +0530

    Time: 0 ms (100%), Space: 44 MB (31.7%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        int count=0,i=0;
+        long miss=1;
+        while(miss<=n)
+        {
+            if(i<nums.length && nums[i]<=miss)
+            {
+                miss+=nums[i++];
+            }
+            else
+            {
+                miss+=miss;
+                count++;
+            }
+        }
+        return count;
+
+        
+    }
+}
