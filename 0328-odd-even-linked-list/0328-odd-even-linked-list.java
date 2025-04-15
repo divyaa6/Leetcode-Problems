f56b6faac1948ff26a861bdfece17bdb5f0f72a0
divyaa6 <165681084+divyaa6@users.noreply.github.com>
2025-04-15 18:17:26 +0530

    Time: 0 ms (100%), Space: 44.4 MB (79.6%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -16,16 +16,13 @@ class Solution {
         ListNode odd=head,eve=head.next,stop=head.next;
 
         while(stop!=null){
-            System.out.println(odd.val);
             odd.next=odd.next.next;
             odd=odd.next;
-            
-            System.out.println(eve.val);
             eve.next=eve.next.next;
             eve=eve.next;
 
             if(odd.next==null || odd.next.next==null){    
-            odd.next=stop;System.out.println(head.next.val);
+            odd.next=stop;
             stop=null;
             }           
                 
